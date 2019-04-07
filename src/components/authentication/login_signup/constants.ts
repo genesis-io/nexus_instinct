4b431d0f26ef8e56afcf7837566b000aa27dd4b7
Mariano Okpalefe <mokpalefe1@gmail.com>
2019-04-07 15:45:55 -0700

    [NXM-2] - create constants for login signup component - update stylings and add primitives to use throughout app - create login signup component

--- src/components/authentication/login_signup/constants.ts
+++ src/components/authentication/login_signup/constants.ts
@@ -1,10 +1,12 @@
-export const formText: any = {
+export const formText: { [key: string]: any } = {
   '/login': {
-  buttonText: 'Log in',
-    header: 'Welcome Back!'
+    buttonText: 'Log in',
+    header: 'Welcome Back!',
+    socialText: 'Log in with GitHub'
   },
   '/signup': {
     buttonText: 'Sign up',
-    header: 'Sign Up!'
+    header: 'Sign Up!',
+    socialText: 'Sign up with GitHub'
   }
 };
--- src/components/authentication/login_signup/index.tsx
+++ src/components/authentication/login_signup/index.tsx
@@ -4,13 +4,20 @@ import
   FunctionComponent,
   useState
 } from 'react';
-
 import { RouteComponentProps } from 'react-router-dom';
+import {
+  Form,
+  PrimaryButton,
+  Input
+} from '../../shared/form_elements';
+import {
+  HLine,
+  Container,
+  GitHubButton
+} from './styles';
 
-import { Form } from './styles';
-import { Button } from '../../shared/form_elements/button';
-import { Input } from '../../shared/form_elements/input';
-import {  Header } from '../../shared/typography/header';
+import { ReactComponent as GitHubLogo } from '../../../assets/github-logo.svg';
+import { Header } from '../../shared/typography';
 import { formText } from './constants';
 
 interface Props extends RouteComponentProps {}
@@ -18,23 +25,35 @@ interface Props extends RouteComponentProps {}
 const Login_Signup: FunctionComponent<Props> = (props) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const constants: any = formText[props.match.url];
-  console.log(props);
+  const constants = formText[props.match.url];
+
+  const handleSubmit = (event: BaseSyntheticEvent) => {
+    event.preventDefault();
+  };
 
   return(
-    <Form>
-      <Header>{constants.header}</Header>
-      <Input
-        value={email}
-        label="email"
-        onChange={(event: BaseSyntheticEvent) => setEmail(event.target.value)}
-      />
-      <Input
-        value={password}
-        label="password"
-        onChange={(event: BaseSyntheticEvent) => setPassword(event.target.value)}/>
-      <Button text={constants.buttonText} type="primary" />
-    </Form>
+    <Container>
+      <Form onSubmit={handleSubmit}>
+        <Header>{constants.header}</Header>
+        <GitHubButton type="button">
+          <GitHubLogo />
+          <span>{constants.socialText}</span>
+        </GitHubButton>
+        <HLine />
+        <Input
+          value={email}
+          label="email"
+          onChange={(event: BaseSyntheticEvent) => setEmail(event.target.value)}
+        />
+        <Input
+          type="password"
+          value={password}
+          label="password"
+          onChange={(event: BaseSyntheticEvent) => setPassword(event.target.value)}
+        />
+        <PrimaryButton type="submit">{constants.buttonText}</PrimaryButton>
+      </Form>
+    </Container>
   );
 };
 
